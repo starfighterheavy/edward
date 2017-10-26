7254442f0ec8a3b645f5a80fc230c66fbb63f745
Jonathan Kirst <jskirst@gmail.com>
2017-10-26 11:27:38 -0700

    CORS configuration had been removed

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,11 @@ module Edward
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :patch, :put, :options]
+      end
+    end
   end
 end
